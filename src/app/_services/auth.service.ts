8c22098fdedb66d39ea21e46f0f00fe01af3ee7f
Adebayo Adesegun Daniel <chamsdev@outlook.com>
2019-02-13 17:29:44 +0100

    End of Session 11

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_modules/user';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +14,15 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   jwthelper = new JwtHelperService();
   decodedToken: any;
+  currentuser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
 constructor(private http: HttpClient) { }
+
+changeMemberPhoto(photoUrl: string) {
+  this.photoUrl.next(photoUrl);
+}
 
 login(model: any) {
         return this.http.post(this.baseUrl + 'login', model)
@@ -22,8 +31,10 @@ login(model: any) {
             const user = response;
             if (user) {
               localStorage.setItem('token', user.token);
+              localStorage.setItem('user', JSON.stringify(user.user));
               this.decodedToken = this.jwthelper.decodeToken(user.token);
-              console.log(this.decodedToken);
+              this.currentuser = user.user;
+              this.changeMemberPhoto(this.currentuser.photoUrl);
             }
           })
         );
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtModule } from '@auth0/angular-jwt';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-chnages.guard';
+import { FileUploadModule } from 'ng2-file-upload';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -58,6 +59,7 @@ export function tokenGetter() {
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
+      FileUploadModule,
       JwtModule.forRoot({
         config: {
           tokenGetter: tokenGetter,
